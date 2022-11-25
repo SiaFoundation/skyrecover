1b4ee10d757df0efdac09349349699a2bb99f962
Nate Maninger <me@n8m.us>
2022-11-25 12:42:59 -0600

    fix small files

--- cmd/metabuild/main.go
+++ cmd/metabuild/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/md5"
 	"crypto/sha256"
 	"crypto/sha512"
@@ -55,34 +56,35 @@ func findMatchingSkyKey(skykeyDB *skykey.SkykeyManager, encryptionIdentifier []b
 }
 
 // parseMetadata parses a base sector and returns the Skyfile metadata.
-func parseMetadata(skykeyDB *skykey.SkykeyManager, skylink, metaPath string) (skymodules.SkyfileMetadata, error) {
+func parseMetadata(skykeyDB *skykey.SkykeyManager, skylink, metaPath string) (skymodules.SkyfileMetadata, []byte, error) {
 	f, err := os.Open(metaPath)
 	if err != nil {
-		return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to open metadata file: %w", err)
+		return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to open metadata file: %w", err)
 	}
 	defer f.Close()
 
 	var sl skymodules.Skylink
 	if err := sl.LoadString(skylink); err != nil {
-		return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to parse skylink: %w", err)
+		return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to parse skylink: %w", err)
 	}
 
 	offset, length, err := sl.OffsetAndFetchSize()
 	if err != nil {
-		return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to get offset and fetch size: %w", err)
+		return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to get offset and fetch size: %w", err)
 	}
 
 	if _, err := f.Seek(int64(offset), io.SeekStart); err != nil {
-		return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to seek to offset %v: %w", offset, err)
+		return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to seek to offset %v: %w", offset, err)
 	}
 
 	baseSector := make([]byte, length)
 	if _, err := io.ReadFull(f, baseSector); err != nil {
-		return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to read base sector: %w", err)
+		return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to read base sector: %w", err)
 	}
 
 	// if the layout is encrypted, decrypt it first
 	if skymodules.IsEncryptedBaseSector(baseSector) {
+		log.Println("base sector is encrypted")
 		var layout skymodules.SkyfileLayout
 		layout.Decode(baseSector)
 
@@ -103,30 +105,30 @@ func parseMetadata(skykeyDB *skykey.SkykeyManager, skylink, metaPath string) (sk
 			masterSkykey, err = findMatchingSkyKey(skykeyDB, keyID[:], nonce)
 		}
 		if err != nil {
-			return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to get skykey: %w", err)
+			return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to get skykey: %w", err)
 		}
 
 		// derive the file-specific key
 		fileSkykey, err := masterSkykey.SubkeyWithNonce(nonce)
 		if err != nil {
-			return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to derive file-specific skykey: %w", err)
+			return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to derive file-specific skykey: %w", err)
 		}
 
 		// derive the base sector subkey and use it to decrypt the base sector
 		baseSectorKey, err := fileSkykey.DeriveSubkey(skymodules.BaseSectorNonceDerivation[:])
 		if err != nil {
-			return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to derive base sector subkey: %w", err)
+			return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to derive base sector subkey: %w", err)
 		}
 
 		// get the cipherkey
 		ck, err := baseSectorKey.CipherKey()
 		if err != nil {
-			return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to get cipherkey: %w", err)
+			return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to get cipherkey: %w", err)
 		}
 
 		_, err = ck.DecryptBytesInPlace(baseSector, 0)
 		if err != nil {
-			return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to decrypt base sector: %w", err)
+			return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to decrypt base sector: %w", err)
 		}
 
 		// save the visible-by-default fields of the baseSector's layout
@@ -147,11 +149,11 @@ func parseMetadata(skykeyDB *skykey.SkykeyManager, skylink, metaPath string) (sk
 
 	// attempt to parse the metadata from the base sector. May return a
 	// recursive base sector error.
-	_, _, meta, _, _, err := skymodules.ParseSkyfileMetadata(baseSector)
+	_, _, meta, _, payload, err := skymodules.ParseSkyfileMetadata(baseSector)
 	if err == nil {
-		return meta, nil
+		return meta, payload, nil
 	} else if err != nil && !strings.Contains(err.Error(), "can't use skymodules.ParseSkyfileMetadata to parse recursive base sector - use renter.ParseSkyfileMetadata instead") {
-		return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to parse base sector: %w", err)
+		return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to parse base sector: %w", err)
 	}
 
 	// Since its a recursive base sector, only parse the layout
@@ -163,11 +165,58 @@ func parseMetadata(skykeyDB *skykey.SkykeyManager, skylink, metaPath string) (sk
 
 	// seek to the start of the JSON payload and parse it
 	if _, err := f.Seek(int64(sectorSize+translatedOffset+layout.FanoutSize), io.SeekStart); err != nil {
-		return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to seek to metadata pos %v: %w", translatedOffset+layout.FanoutSize, err)
+		return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to seek to metadata pos %v: %w", translatedOffset+layout.FanoutSize, err)
 	} else if err := json.NewDecoder(f).Decode(&meta); err != nil {
-		return skymodules.SkyfileMetadata{}, fmt.Errorf("failed to decode metadata: %w", err)
+		return skymodules.SkyfileMetadata{}, nil, fmt.Errorf("failed to decode metadata: %w", err)
 	}
-	return meta, nil
+	return meta, nil, nil
+}
+
+// recoverFiles recovers the files from the metadata and saves them in outputDir
+func recoverFiles(r io.ReadSeeker, meta skymodules.SkyfileMetadata, outputDir, algo string) {
+	// pipe the -extended data to a hasher to calculate the checksum
+	var h hash.Hash
+	switch strings.ToLower(algo) {
+	case "sha256":
+		h = sha256.New()
+	case "sha512":
+		h = sha512.New()
+	case "md5":
+		h = md5.New()
+	default:
+		log.Fatalln("unknown checksum algorithm:", algo)
+	}
+
+	tr := io.TeeReader(r, h)
+	if len(meta.Subfiles) == 0 {
+		log.Println("Found 1 file")
+		outPath := filepath.Join(outputDir, meta.Filename)
+		if err := writeSubFile(tr, outPath, int64(meta.Length)); err != nil {
+			log.Fatalln("failed to write file:", err)
+		}
+		log.Printf("Recovered file %v (%v/%v) %v bytes %x checksum", meta.Filename, 1, 1, meta.Length, h.Sum(nil))
+		return
+	}
+
+	log.Printf("Found %v files", len(meta.Subfiles))
+
+	var i int
+	n := len(meta.Subfiles)
+	for _, subfile := range meta.Subfiles {
+		i++
+		// reset the hasher
+		h.Reset()
+		// seek to the file offset in the -extended file
+		if _, err := r.Seek(int64(subfile.Offset), io.SeekStart); err != nil {
+			log.Fatalln("failed to seek to subfile:", err)
+		}
+		// write the subfile to disk and calculate its sha256 checksum
+		outPath := filepath.Join(outputDir, subfile.Filename)
+		if err := writeSubFile(tr, outPath, int64(subfile.Len)); err != nil {
+			log.Fatalln("failed to write subfile:", err)
+		}
+		log.Printf("Recovered file %v (%v/%v) %v bytes %x checksum", subfile.Filename, i, n, subfile.Len, h.Sum(nil))
+	}
 }
 
 func main() {
@@ -186,9 +235,16 @@ func main() {
 	}
 
 	// parse the skyfile metadata from the -base file
-	meta, err := parseMetadata(skykeyDB, *skylink, *basePath)
+	meta, payload, err := parseMetadata(skykeyDB, *skylink, *basePath)
 	if err != nil {
 		log.Fatalln("failed to parse base sectors:", err)
+	}
+
+	// the entire payload is in the base sector, recover files from it
+	if uint64(len(payload)) == meta.Length {
+		log.Println("base sector contains entire payload")
+		recoverFiles(bytes.NewReader(payload), meta, *outputDir, *checksumAlgo)
+		return
 	}
 
 	// check that the -extended file is the correct size
@@ -205,49 +261,6 @@ func main() {
 		log.Fatalln("failed to open extended sector:", err)
 	}
 
-	// pipe the -extended data to a hasher to calculate the checksum
-	var h hash.Hash
-	switch strings.ToLower(*checksumAlgo) {
-	case "sha256":
-		h = sha256.New()
-	case "sha512":
-		h = sha512.New()
-	case "md5":
-		h = md5.New()
-	default:
-		log.Fatalln("unknown checksum algorithm:", *checksumAlgo)
-	}
-
-	tr := io.TeeReader(ef, h)
-
-	// if there are no subfiles, the -extended file should be the full raw data
-	if len(meta.Subfiles) == 0 {
-		log.Println("Found 1 file")
-		outPath := filepath.Join(*outputDir, meta.Filename)
-		if err := writeSubFile(tr, outPath, int64(meta.Length)); err != nil {
-			log.Fatalln("failed to write file:", err)
-		}
-		log.Printf("Recovered file %v (%v/%v) %v bytes %x checksum", meta.Filename, 1, 1, meta.Length, h.Sum(nil))
-		return
-	}
-
-	log.Printf("Found %v files", len(meta.Subfiles))
-
-	var i int
-	n := len(meta.Subfiles)
-	for _, subfile := range meta.Subfiles {
-		i++
-		// reset the hasher
-		h.Reset()
-		// seek to the file offset in the -extended file
-		if _, err := ef.Seek(int64(subfile.Offset), io.SeekStart); err != nil {
-			log.Fatalln("failed to seek to subfile:", err)
-		}
-		// write the subfile to disk and calculate its sha256 checksum
-		outPath := filepath.Join(*outputDir, subfile.Filename)
-		if err := writeSubFile(tr, outPath, int64(subfile.Len)); err != nil {
-			log.Fatalln("failed to write subfile:", err)
-		}
-		log.Printf("Recovered file %v (%v/%v) %v bytes %x checksum", subfile.Filename, i, n, subfile.Len, h.Sum(nil))
-	}
+	// recover the files from the -extended file
+	recoverFiles(ef, meta, *outputDir, *checksumAlgo)
 }
